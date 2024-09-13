737f3a94d04b9bdcdc505bc41b02eccb1446adb6
Hoang Thuy - Desktop <h.thuy7605@gmail.com>
2024-09-14 00:22:51 +0700

    Update Lab 1
    Signed-off-by: Hoang Thuy - Desktop <h.thuy7605@gmail.com>

--- src/main/java/com/poly/lab1/bai1.java
+++ src/main/java/com/poly/lab1/bai1.java
@@ -8,12 +8,10 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 @WebServlet("/bai1")
-public class Bai1 extends HttpServlet{
+public class bai1 extends HttpServlet{
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		req.getRequestDispatcher("bai1.jsp").forward(req, resp);
-		
 	}
-
 }
