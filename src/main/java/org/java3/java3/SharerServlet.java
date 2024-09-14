1a30d4c04a3efff6710f97c5b3ed0a65cc7168d2
Hoang Thuy - Laptop <h.thuy7605@gmail.com>
2024-09-14 10:21:11 +0700

    Update lab2
    Signed-off-by: Laptop <h.thuy7605@gmail.com>
    Signed-off-by: Admin <h.thuy7605@gmail.com>

---
+++ src/main/java/org/java3/java3/SharerServlet.java
@@ -0,0 +1,17 @@
+package org.java3.java3;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.*;
+import jakarta.servlet.http.*;
+
+import java.io.IOException;
+import java.util.Date;
+
+@WebServlet("/sharerServlet")
+public class SharerServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.setAttribute("message", "Welcome to FPT Polytechnic");
+        request.setAttribute("now", new Date());
+        request.getRequestDispatcher("page.jsp").forward(request, response);
+    }
+}
