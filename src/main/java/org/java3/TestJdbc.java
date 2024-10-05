ec64d9790e95ca07cc49ce74b4c3d1837d79834d
Hoang Thuy - Laptop <h.thuy7605@gmail.com>
2024-10-05 10:18:15 +0700

    Update ASM

--- src/main/java/org/java3/TestJdbc.java
+++ src/main/java/org/java3/TestJdbc.java
@@ -6,13 +6,21 @@ import org.java3.entity.News;
 public class TestJdbc {
     public static void main(String[] args) {
         NewsDAO newsDAO = new NewsDAO();
-        for (News news : newsDAO.selectAll()) {
-            System.out.println(news.getId() + " - " + news.getContent() + " - " + news.getCategories_id());
-
+        String[] news_id = {"20240919093010", "20240920121525", "20240920183055"};
+        for (String s : news_id) {
+            News news = newsDAO.selectById(s);
+            System.out.println(news.getId() + " - " + news.getContent() + " - " + news.getImg() + " - " + news.getView_count());
         }
-        News news1 = newsDAO.selectById("20240919093010");
-        news1.setImg("tphcmgido");
-        newsDAO.update(news1);
-        System.out.println(newsDAO.selectById("20240919093010").getImg());
+        System.out.println();
+        for (String s : news_id) {
+            News news = newsDAO.selectById(s);
+            news.setView_count(news.getView_count() + 1);
+            newsDAO.update(news);
+            System.out.println(news.getId() + " - " + news.getContent() + " - " + news.getCategories_id() + " - " + news.getImg() + " - " + news.getView_count());
+        }
+//        News news1 = newsDAO.selectById("20240919093010");
+//        news1.setImg("tphcmgido");
+//        newsDAO.update(news1);
+//        System.out.println(newsDAO.selectById("20240919093010").getImg());
     }
 }
