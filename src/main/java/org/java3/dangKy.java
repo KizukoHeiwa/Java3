d8fa89b8c6490a26a85af3619df121897c1b0494
Admin <h.thuy7605@gmail.com>
2024-10-12 10:01:57 +0700

    Update ASM

---
+++ src/main/java/org/java3/dangKy.java
@@ -0,0 +1,36 @@
+package org.java3;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.java3.dao.UsersDAO;
+import org.java3.entity.Users;
+import org.java3.utils.XDate;
+
+import java.io.IOException;
+
+@WebServlet("/dangKy")
+public class dangKy extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getRequestDispatcher("/dangKy.jsp").forward(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Users user = new Users();
+        user.setId(req.getParameter("username"));
+        user.setPassword(req.getParameter("password"));
+        user.setFullname(req.getParameter("hoTen"));
+        user.setFullname(req.getParameter("fullname"));
+        user.setBirthday(XDate.toDate(req.getParameter("birthday"), "dd/MM/yyyy"));
+        user.setGender(req.getParameter("gender").equals("Nam"));
+        user.setMobile(req.getParameter("mobile"));
+        user.setEmail(req.getParameter("email"));
+
+        new UsersDAO().insert(user);
+        resp.sendRedirect(req.getContextPath() + "/dangNhap");
+    }
+}
